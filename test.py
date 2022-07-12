c4006e51a344b545c28c3b06824095f865fc5a9c
Marco <marcoabustam@gmail.com>
2022-07-11 22:32:20 -0400

    [IMP] pequeños ajustes

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 import string
 
 """
-repo: 
+repo: https://github.com/marcobustamanteab/test-python-para-falabella.git
 
 -importamos String para poder usar ascii_uppercase y lo guardamos en la variable alphabet
 -declaramos un diccionario vacio llamado 'patentes' e inicializamos la id en 1
 -iteramos con letter la variable que contiene el alfabeto y anidamos con otra iteración para los números
 -luego de condicionar el formato de texto a retornar, le asignamos ese valor a la clave id 'patentes[id]'
 -sumamos += 1 a ID para que continue con la siguiente iteracion hasta llegar a la última ID
--guardamosa en la variable 'find_by_key' el valor de la ID ingresada por teclado
+-guardamos en la variable 'find_by_key' el valor de la ID ingresada por teclado
 -guardamos en result el valor obtenido a través del método .get de nuestro diccionario y condicionamos para ver si encuentra o no el valor asociado a la clave
 -guardamos en 'find_by_value' el valor de la patente ingresada por teclado y condicionamos para comparar los valores de nuestro diccionario y el texto ingresado
 
@@ -37,14 +37,15 @@ find_by_key = int(input("Ingresa el ID de la patente: "))
 result = patentes.get(find_by_key)
 
 if result:
-    	print("La patente es:", result)
+    print(f"La patente es: {result}")
 else:
-    	print("No existe una patente asociada con ese ID")
-    	
+    print("No existe una patente asociada con ese ID")
+
 find_by_value = input("Ingresa la patente: ")
+
 if find_by_value in list(patentes.values()):
 	index = list(patentes.values()).index(find_by_value)
 	id = list(patentes.keys())[index]
-	print("El ID es:", id)
+	print(f"El ID es: {id}")
 else:
 	print("No se encuentran registros de la patente....")
