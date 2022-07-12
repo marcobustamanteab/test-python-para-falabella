6d0389ac2d7330c9823a9aef910e91d20c569f2a
Marco <marcoabustam@gmail.com>
2022-07-11 22:25:54 -0400

    [IMP] agregamos comentario del código

--- test.py
+++ test.py
@@ -1,5 +1,20 @@
 import string
 
+"""
+repo: 
+
+-importamos String para poder usar ascii_uppercase y lo guardamos en la variable alphabet
+-declaramos un diccionario vacio llamado 'patentes' e inicializamos la id en 1
+-iteramos con letter la variable que contiene el alfabeto y anidamos con otra iteración para los números
+-luego de condicionar el formato de texto a retornar, le asignamos ese valor a la clave id 'patentes[id]'
+-sumamos += 1 a ID para que continue con la siguiente iteracion hasta llegar a la última ID
+-guardamosa en la variable 'find_by_key' el valor de la ID ingresada por teclado
+-guardamos en result el valor obtenido a través del método .get de nuestro diccionario y condicionamos para ver si encuentra o no el valor asociado a la clave
+-guardamos en 'find_by_value' el valor de la patente ingresada por teclado y condicionamos para comparar los valores de nuestro diccionario y el texto ingresado
+
+"""
+
+
 alphabet = list(string.ascii_uppercase)
 
 patentes = {}
